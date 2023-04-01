aca0e79738589a37f2dd032233cf9db155b02b1e
Marta Pabierowska <marta.cipher@gmail.com>
2023-04-01 19:58:40 +0200

    adding new tasks and showing if a task is done

---
+++ js/script.js
@@ -0,0 +1,65 @@
+{
+    const welcome = () => {
+        console.log("WELCOME!");
+    }
+
+    welcome();
+
+    const tasks = [
+        {
+            content: "test",
+            done: false,
+        },
+        {
+            content: "test1",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                  ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
+                  ${task.content}
+                </li>
+              `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+};
+
+init();
+}
